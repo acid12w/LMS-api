79d0724637663d96eb32dcb46fe623a169fa691f
Adrian Anderson <adriananderson@Machaelas-MacBook-Air.local>
2024-03-07 23:18:37 -0500

    npm start update

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -8,7 +8,7 @@ import {
     MaxFileSizeValidator,
     FileTypeValidator } from '@nestjs/common';
     import { FileInterceptor } from '@nestjs/platform-express';
-import { SharpPipe } from './sharp.pipe';
+// import { SharpPipe } from './sharp.pipe';
 import { UploadService } from './upload.service'; 
 
 
@@ -18,7 +18,8 @@ export class UploadController {
 
   @Post()
   @UseInterceptors(FileInterceptor('file'))
-  async uploadImage(@UploadedFile(SharpPipe) file: Express.Multer.File){   
+  // async uploadImage(@UploadedFile(SharpPipe) file: Express.Multer.File){   
+  async uploadImage(@UploadedFile() file: Express.Multer.File){   
       const result = await this.uploadService.uploadCourseThumbnail(file); 
       return result;
     }
